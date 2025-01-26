d26cb79c20bdd1adf8fc2d188dc9103744aeb974
urastogi74 <umangrastogi74@gmail.com>
2025-01-26 16:40:58 +0000

    jj

--- examples/index.js
+++ examples/index.js
@@ -1,53 +1,14 @@
-const express = require('express');
-const app = express();
-const request = require('request');
-const wikip = require('wiki-infobox-parser');
+const http = require('http');
 
-//ejs
-app.set("view engine", 'ejs');
+const hostname = '0.0.0.0'; // Listen on all interfaces inside Docker
+const port = 3000;
 
-//routes
-app.get('/', (req,res) =>{
-    res.render('index');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from Dockerized Node.js app!');
 });
 
-app.get('/index', (req,response) =>{
-    let url = "https://en.wikipedia.org/w/api.php"
-    let params = {
-        action: "opensearch",
-        search: req.query.person,
-        limit: "1",
-        namespace: "0",
-        format: "json"
-    }
-
-    url = url + "?"
-    Object.keys(params).forEach( (key) => {
-        url += '&' + key + '=' + params[key]; 
-    });
-
-    //get wikip search string
-    request(url,(err,res, body) =>{
-        if(err) {
-            response.redirect('404');
-        }
-            result = JSON.parse(body);
-            x = result[3][0];
-            x = x.substring(30, x.length); 
-            //get wikip json
-            wikip(x , (err, final) => {
-                if (err){
-                    response.redirect('404');
-                }
-                else{
-                    const answers = final;
-                    response.send(answers);
-                }
-            });
-    });
-
-    
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-//port
-app.listen(3000, console.log("Listening at port 3000..."))
