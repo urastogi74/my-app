52a56c40c2b562e55b35c695da9201b509e05632
urastogi74 <umangrastogi74@gmail.com>
2025-01-26 21:05:20 +0530

    Update app.py

--- examples/app.py
+++ examples/app.py
@@ -1,5 +1,14 @@
-FROM ubuntu:20.04
-RUN apt-get update && apt-get install -y python3 python3-pip
-RUN pip3 install flask
-COPY app.py /opt/
-ENTRYPOINT FLASK_APP=/opt/app.py flask run --host=0.0.0.0 --port=8080
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
